6d5756db11c2f180af93a595cd694a77d01d5291
Gage Newman <gagesnewman@gmail.com>
2017-05-24 13:32:24 -0700

    write out plans for search

--- frontend/components/AppNav.jsx
+++ frontend/components/AppNav.jsx
@@ -7,3 +7,15 @@ export default () => (
       className="input--search"/>
   </nav>
 )
+
+// on search (livereload on keypress)
+  // pop-down menu of matching terms
+    // artists
+    // albums
+    // songs
+    // "See All"
+
+// Search reducer with the above keys
+// Search menu component
+// Search page component (for "See All")
+  // uses Artists, Albums, & Songs?
