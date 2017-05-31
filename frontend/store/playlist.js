5671e35f33085d72a9341d36cc3eb20b3858d174
Gage Newman <gagesnewman@gmail.com>
2017-05-31 08:56:54 -0700

    fix issue with playlist middleware

--- frontend/store/playlist.js
+++ frontend/store/playlist.js
@@ -6,7 +6,7 @@ export default ({ dispatch, getState }) => next => action => {
     case "NEXT_SONG":
       const songId = getState().currentQueue[0]
       if(songId)
-        dispatch(startSong(getState().songs[songId]))
+        dispatch(startSong(getState().songs.all[songId]))
       break
     case "PREV_SONG":
       // const song = getState().reverseQueue[0]
