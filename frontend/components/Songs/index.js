b91ada37725a2b08bf458dbbc36eac7022e95b83
Gage Newman <gagesnewman@gmail.com>
2017-06-22 17:10:10 -0700

    clean out deprecated code

--- frontend/components/Songs/index.js
+++ frontend/components/Songs/index.js
@@ -14,33 +14,3 @@ export default () => (
     <SongsListContainer/>
   </article>
 )
-
-// deprecated code (when this component requested all the songs)
-//
-// class Songs extends React.Component {
-//   render() {
-//     return (
-//       <article className="article--songs">
-//         <SongsHeader/>
-//         <SongsListContainer/>
-//       </article>
-//     )
-//   }
-//
-//   componentDidMount() {
-//     this.props.requestAllSongs()
-//   }
-// }
-//
-// const mapDispatchToProps = (dispatch) => ({
-//   requestAllSongs: () => dispatch(requestAllSongs())
-// })
-//
-// export default connect(null, mapDispatchToProps)(Songs)
-
-// export default () => (
-//   <article className="article--songs">
-//     <SongsHeader/>
-//     <SongsListContainer/>
-//   </article>
-// )
